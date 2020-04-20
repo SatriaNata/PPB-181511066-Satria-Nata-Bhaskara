0563ca41f565f9efadb5eecdc476e08742ec265c
SatriaNata <natasatria371@gmail.com>
2020-04-20 12:32:07 +0700

    SMS

---
+++ app/src/main/java/com/task4snb/sms/SMSReceiver.java
@@ -0,0 +1,32 @@
+package com.task4snb.sms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsMessage; import android.util.Log;
+import android.widget.Toast;
+
+public class SMSReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        //---get the SMS message passed in---
+        Bundle bundle = intent.getExtras(); SmsMessage[] msgs = null;
+        String str = "SMS from 5554/082115222339 "; if (bundle != null)
+        {
+            //---retrieve the SMS message received---
+            msgs = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+            for (int i=0; i<msgs.length; i++){
+            str += msgs[i].getMessageBody().toString();
+            }
+            //---get the message body---
+            //str += msgs[i].getMessageBody().toString();
+        }
+        //---display the new SMS message---
+        Toast.makeText(context, str, Toast.LENGTH_SHORT).show(); Log.d("SMSReceiver", str);
+    }
+}
+
